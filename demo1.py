696ea35a0f6c2e05b129795cfbb9b674a5f94cee
Abid-Shabbir05 <abid.mlmind@gmail.come>
2025-02-18 21:15:14 +0500

    update demo1.py with new code

--- demo1.py
+++ demo1.py
@@ -1,3 +1,10 @@
 print("hello")
 arr = [12,23,24]
 print('array', arr)
+
+def factorial(n):
+    if n ==0 or n==1:
+        return 1
+    return n*factorial(n -1)
+num = 5
+print(f"factorial of {num} is {factorial(num)}")
